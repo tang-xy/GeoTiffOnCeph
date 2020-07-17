c04aaea54783af6a07eaee17853a211dcf0bad0c
tang-xy <a123654789654@yeah.net>
2020-07-18 00:30:34 +0800

    测试

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from pyspark import SparkContext
 logFile = "file://///root/GeoTiffOnCeph/readme.md"
 sc = SparkContext("local", "first app")
 logData = sc.textFile(logFile).cache()
-print(logData)
-# numAs = logData.filter(lambda s: 'a' in s).count()
-# numBs = logData.filter(lambda s: 'b' in s).count()
-# print("Line with a:%i,lines with b :%i" % (numAs, numBs))
+# print(logData)
+numAs = logData.filter(lambda s: 'a' in s).count()
+numBs = logData.filter(lambda s: 'b' in s).count()
+print("Line with a:%i,lines with b :%i" % (numAs, numBs))
