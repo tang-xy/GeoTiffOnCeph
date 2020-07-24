40876b43bf77dc8692d0230a2808193fd5c5bed4
tang-xy <a123654789654@yeah.net>
2020-07-24 20:16:00 +0800

    测试

--- WriteS3.py
+++ WriteS3.py
@@ -50,15 +50,16 @@ def RsImage(filepath):
     dic['im_width'] = dataset.RasterXSize
     dic['im_height'] = dataset.RasterYSize
     bandda = dataset.GetRasterBand(1)
-    dic['dayData'] = bandda.ReadRaster(0, 0, dic['im_width'], dic['im_height'])#.tobytes().decode('utf-8')
+    #dic['dayData'] = bandda.ReadRaster(0, 0, dic['im_width'], dic['im_height'])#.tobytes().decode('utf-8')
+    img = bandda.ReadRaster(0, 0, dic['im_width'], dic['im_height'])
     dic['im_geotrans'] = dataset.GetGeoTransform()
     dic['im_proj'] = dataset.GetProjection()
     del dataset
-    return dic
+    return dic, img
 
 if __name__ == "__main__":
     # boto3
-    #ephs3_boto3 = CephS3BOTO3()
+    cephs3_boto3 = CephS3BOTO3()
     #cephs3_boto3.get_bucket()
     #cephs3_boto3.create_bucket('mosic2003')
     start = time()
@@ -66,14 +67,17 @@ if __name__ == "__main__":
     for root, dirs, files in os.walk('mosic2003'):
         for di in dirs:
             path = os.path.join(root, di)
-            day_image = RsImage(path + '\\day\\result.tif')
-            night_image = RsImage(path + '\\night\\result.tif')
-            strtemp = json.dumps(day_image)
-            dictemp = json.loads(strtemp)
-            #cephs3_boto3.upload(di + 'day', day_image)
-            #cephs3_boto3.upload(di + 'night', night_image)
+            day, day_image = RsImage(path + '\\day\\result.tif')
+            night, night_image = RsImage(path + '\\night\\result.tif')
+            strtemp = json.dumps(day)
+            cephs3_boto3.upload(di + 'day_img', day_image)
+            cephs3_boto3.upload(di + 'day', strtemp)
+            strtemp = json.dumps(night)
+            cephs3_boto3.upload(di + 'night_img', night_image)
+            cephs3_boto3.upload(di + 'night', strtemp)
+            #dictemp = json.loads(strtemp)
             #cephs3_boto3.upload('ts', night_image.dayData.tobytes())
-            break
+            
         break
     stop = time()
     print("Stop: " + str(stop))
