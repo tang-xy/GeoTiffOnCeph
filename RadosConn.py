8ddaae0ecbfd720101718d8c728ca0dfd3d0ca3a
tang-xy <a123654789654@yeah.net>
2020-07-25 22:57:29 +0800

    测试

--- RadosConn.py
+++ RadosConn.py
@@ -59,7 +59,7 @@ class RadosConn():
         dic['im_height'] = dataset.RasterYSize
         bandda = dataset.GetRasterBand(1)
         #dic['dayData'] = bandda.ReadRaster(0, 0, dic['im_width'], dic['im_height'])#.tobytes().decode('utf-8')
-        img = bandda.ReadRaster(0, 0, dic['im_width'], dic['im_height'])
+        img = bandda.ReadAsArray(0, 0, dic['im_width'], dic['im_height'])
         dic['im_geotrans'] = dataset.GetGeoTransform()
         dic['im_proj'] = dataset.GetProjection()
         del dataset
