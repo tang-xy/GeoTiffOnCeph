9c37adc964601ff5c689354c96c25aab3daa7b1a
tang-xy <a123654789654@yeah.net>
2020-08-20 17:58:51 +0800

    测试

--- TestSQL.py
+++ TestSQL.py
@@ -2,16 +2,20 @@
 import sys, re
 from operator import add
 from pyspark.sql import SparkSession
-from pyspark.conf import SparkConf
   
-if __name__ == "__main__":
-    spark = SparkSession\
-            .builder\
-            .getOrCreate()
-    sc = spark.sparkContext
-    a = sc.parallelize([1, 2, 3])
-    b = a.flatMap(lambda x: (x,x ** 2))
-    print(a.collect())
-    print(b.collect())
+spark = SparkSession\
+  .builder\
+  .appName("PythonWordCount")\
+  .getOrCreate()
 
-    spark.stop()
+# Access the file  
+lines = spark.read.text("/tmp/test.txt").rdd.map(lambda r: r[0])
+# counts = lines.flatMap(lambda x: x.split(',')) \
+#   .map(lambda x: (x, 1)) \
+#   .reduceByKey(add) \
+#   .sortBy(lambda x: x[1], False)
+# output = counts.collect()
+# for (word, count) in output:
+#   print("%s: %i" % (word, count))
+
+spark.stop()
