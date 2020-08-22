98a7248f173b4cfb07cbfe7b1b5c8dd731d3043c
tang-xy <a123654789654@yeah.net>
2020-08-22 15:47:44 +0800

    修复继承

--- HdfsEditor.py
+++ HdfsEditor.py
@@ -3,5 +3,5 @@ from hdfs import InsecureClient
 
 class HdfsEditor(InsecureClient):
     def __init__(self):
-        super().__init__('http://instance-2:9870',user='hdfs')
+        super(HdfsEditor, self).__init__('http://instance-2:9870',user='hdfs')
         
