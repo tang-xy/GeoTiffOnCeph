a624664f322225ddd04026e0b46581c819de0732
tang-xy <a123654789654@yeah.net>
2020-10-15 22:11:33 +0800

    全局transport

--- test/TestHBase.py
+++ test/TestHBase.py
@@ -60,7 +60,7 @@ def create_table():
 def update_data():
     start = time()
     print("Start: " + str(start))
-    global client
+    global transport
     transport.open()
     global client
     if client.isTableEnabled('image') == False:
