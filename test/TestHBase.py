e8ad3354129d91e0723c0cd5a53b1d383dca7c50
tang-xy <a123654789654@yeah.net>
2020-10-15 22:03:55 +0800

    读入image

--- test/TestHBase.py
+++ test/TestHBase.py
@@ -84,7 +84,7 @@ def upload_hbase(path):
         #ceph_editor.upload_file(path, 'new_' + basename, meta_dict = meta_dict)
     mutations = [Mutation(column = basename + "_" + k, value=meta_dict[k]) for k in meta_dict]
     with open(path, 'rb') as image:
-        mutations.append(Mutation(column="gf1_data", value=image))
+        mutations.append(Mutation(column="gf1_data", value=image.read()))
     client.mutateRow('image', basename, mutations)
 
 if __name__ == "__main__":
