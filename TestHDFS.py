7e8e8fee550e3462363a8c8149111ccb1e9330e8
tang-xy <a123654789654@yeah.net>
2020-10-16 15:10:51 +0800

    修改测试规模

--- TestHDFS.py
+++ TestHDFS.py
@@ -12,7 +12,7 @@ def createtif(filepath):
     filename, fileend = os.path.splitext(filepath)
     if fileend == '.tfw':
         with open(filename + ".tif","w+") as tiffile:
-            tiffile.write(str([i for i in range(800000)]))
+            tiffile.write(str([i for i in range(80000)]))
 
 
 def do_foreach_file(url, func, end_name = ''):
