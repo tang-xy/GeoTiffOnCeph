893bc589d8a08ef3474399583403ab22dac8f700
tang-xy <a123654789654@yeah.net>
2020-10-16 21:20:43 +0800

    重新上传

--- test/TestHBase.py
+++ test/TestHBase.py
@@ -101,7 +101,7 @@ def upload_hbase(path):
     mutations = []
     with open(path, 'rb') as image:
         #mutations.append(Mutation(column="gf1:data", value=str([i for i in range(80000)])))
-        mutations.append(Mutation(column="gf1_data", value=image.read()))
+        mutations.append(Mutation(column="gf1:data", value=image.read()))
     client.mutateRow('image', basename, mutations)
     transport.close()
 
